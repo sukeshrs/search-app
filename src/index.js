5ef801668e1a860a7af9749670f0f7ffbe360976
sukeshrs <sukesrs@gmail.com>
2018-01-06 11:46:20 -0800

    Converted the base component to a class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
@@ -6,19 +6,21 @@ import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyD2ulV9KPblWPH5zP2l3SBGLMsXvPDamXc';
 
 
-YTSearch({key:API_KEY , term : 'dogs'}, function(data){
+YTSearch({ key: API_KEY, term: 'dogs' }, function (data) {
     console.log(data);
 });
 
 //Create a new component. this should produce some HTML
-const App = () => {
-    return(
-        <div>
-             <SearchBar/>
-        </div>
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
         );
+    }
 }
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
 //Take these component`s generated HTML and put in on
 // the page(in the DOM)
