cac0c0a45660735e6f30a9877505625523dd178c
sukeshrs <sukesrs@gmail.com>
2018-01-06 16:30:49 -0800

    Updated the Root component to read the service in the constructor . Added code to render child components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyD2ulV9KPblWPH5zP2l3SBGLMsXvPDamXc';
 
 
-YTSearch({ key: API_KEY, term: 'dogs' }, function (data) {
-    console.log(data);
-});
+
 
 //Create a new component. this should produce some HTML
 class App extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {videos : []};
+
+        YTSearch({ key: API_KEY, term: 'dogs' },  (data) => {
+            this.setState({videos : data});
+            console.log(this.state.videos);
+        });
+    }
+
+
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
